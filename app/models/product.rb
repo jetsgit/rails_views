321845f504aa447452a818cab7ac393a2b52b689
Jerrold Thompson <jet@whidbey.com>
2015-01-09 22:20:59 -0800

    Completion of simple_form implementation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   belongs_to :publisher
   has_many :categorizations 
   has_many :categories, through: :categorizations 
+  validates_presence_of :name, on: :create, message: "Must provide name"
+  validates_presence_of :price, on: :create, message: "Price can't be blank"
 end
